6d80d24df4dfb48a4f13c43316169ea2d93898df
Jason Sjambar <realjase@gmail.com>
2016-04-28 15:43:56 +0200

    Exercise2

--- PracticeExercises/PracticeExercises/Program.cs
+++ PracticeExercises/PracticeExercises/Program.cs
@@ -6,56 +6,11 @@ using System.Threading.Tasks;
 
 namespace PracticeExercises
 {
-
-    class NumberVisitor : INumberVisitor
-    {
-        public void onMyFloat(MyFloat myfloat)
-        {
-            Console.WriteLine("Found float.");
-        }
-
-        public void onMyInt(MyInt myint)
-        {
-            Console.WriteLine("Found int.");
-        }
-    }
-
-    class MyFloat : INumber
-    {
-        public void Visit(INumberVisitor visitor)
-        {
-            visitor.onMyFloat(this);
-        }
-    }
-
-    class MyInt : INumber   
-    {
-        public void Visit(INumberVisitor visitor)
-        {
-            visitor.onMyInt(this);
-        }
-    }
-
-    interface INumberVisitor
-    {
-        void onMyInt(MyInt myint);
-        void onMyFloat(MyFloat myfloat);
-
-    }
-
-    interface INumber
-    {
-        void Visit(INumberVisitor visitor);
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            NumberVisitor NumberVisitor = new NumberVisitor();
-            NumberVisitor.onMyFloat(new MyFloat());
-            NumberVisitor.onMyInt(new MyInt());
-
+            Console.WriteLine("hoi");
         }
     }
 }
---
+++ PracticeExercises2/PracticeExercises2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeExercises2
+{
+
+    interface IMusicLibraryVisitor
+    {
+        void onHeavyMetal(HeavyMetal heavymetal);
+        void onJazz(Jazz jazz);
+    }
+
+    interface ISong
+    {
+        void Visit(IMusicLibraryVisitor musiclibraryvisitor);
+    }
+
+    class Jazz : ISong
+    {
+        public void Visit(IMusicLibraryVisitor musiclibraryvisitor)
+        {
+            musiclibraryvisitor.onJazz(this);
+        }
+    }
+
+    class HeavyMetal : ISong
+    {
+        public void Visit(IMusicLibraryVisitor musiclibraryvisitor)
+        {
+            musiclibraryvisitor.onHeavyMetal(this);
+        }
+    }
+
+    class MusicLibraryVisitor : IMusicLibraryVisitor
+    {
+        List<Jazz> jazzList = new List<Jazz>();
+        List<HeavyMetal> hmList = new List<HeavyMetal>();
+
+        public MusicLibraryVisitor(List<Jazz> jazzList, List<HeavyMetal> hmList)
+        {
+            this.jazzList = jazzList;
+            this.hmList = hmList;
+        }
+
+        public void onHeavyMetal(HeavyMetal heavymetal)
+        {
+            Console.WriteLine(hmList.Count);
+        }
+
+        public void onJazz(Jazz jazz)
+        {
+            Console.WriteLine(jazzList.Count);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Jazz> jazzL = new List<Jazz>();
+            List<HeavyMetal> hmL = new List<HeavyMetal>();
+
+            jazzL.Add(new Jazz());
+            jazzL.Add(new Jazz());
+            jazzL.Add(new Jazz());
+
+            hmL.Add(new HeavyMetal());
+            hmL.Add(new HeavyMetal());
+            hmL.Add(new HeavyMetal());
+            hmL.Add(new HeavyMetal());
+            hmL.Add(new HeavyMetal());
+            hmL.Add(new HeavyMetal());
+            hmL.Add(new HeavyMetal());
+
+            MusicLibraryVisitor visitor = new MusicLibraryVisitor(jazzL, hmL);
+
+            visitor.onJazz(new Jazz());
+            visitor.onHeavyMetal(new HeavyMetal());
+
+            Console.Read();
+        }
+    }
+}
